a3e8606b309aa4f33a4fc91bdee763ae95db98ca
= <=>
2019-11-04 18:53:29 +0800

    ;

--- 测试未命名文件夹/cesGit/cesGit/AppDelegate.m
+++ 测试未命名文件夹/cesGit/cesGit/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     @"stadasshas11111";
-    @"-am  666666"
+    @"-am  666666";
 
     return YES;
 }
