f20bef62259e9e5263181dd245dc715a2f0b7194
= <=>
2019-11-04 18:11:40 +0800

    stas

--- 测试未命名文件夹/cesGit/cesGit/AppDelegate.m
+++ 测试未命名文件夹/cesGit/cesGit/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
+    @"stadassh";
     return YES;
 }
 -(void)application{
